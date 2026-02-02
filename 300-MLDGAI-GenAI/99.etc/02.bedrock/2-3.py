cc2161bd65b7f98044b474aad4ad097f8690a327
moonjukhim <50357315+moonjukhim@users.noreply.github.com>
2026-02-02 13:51:01 +0900

    Delete 2-3.py

--- 300-MLDGAI-GenAI/99.etc/02.bedrock/2-3.py
+++
@@ -1,9 +0,0 @@
-from langchain_aws import BedrockLLM
-
-# Bedrock 모델 설정
-llm = BedrockLLM(model_id="anthropic.claude-v2:1")
-
-# 모델을 호출하여 응답 받기
-response = llm.invoke("랭체인으로 LLM 활용하기 참 쉽죠?")
-
-print(response)
